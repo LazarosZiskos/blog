6c5b3d614c61c137d7daa04359eaace60d239d51
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2025-02-27 21:40:12 +0200

    finished styling navbar links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,12 @@ import React from "react";
 import { navLinks } from "@/constants";
 import Link from "next/link";
 import { ThemeChanger } from "./ThemeChanger";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <nav className="py-5">
       <div className="flex justify-evenly items-center text-center">
@@ -16,10 +20,16 @@ const Navbar = () => {
           </a>
         </div>
         <div>
-          <ul className="flex space-x-20 font-semibold">
+          <ul className="flex space-x-20 font-semibold py-2 px-2">
             {navLinks.map((link) => (
-              <Link key={link.id} href={link.href}>
-                <li>{link.title}</li>
+              <Link
+                key={link.id}
+                href={link.href}
+                className={pathname === link.href && "bg-[#F2F2F6] rounded-sm"}
+              >
+                <li className="py-2 px-2 hover:bg-[#F2F2F6] rounded-sm leading-[19px] transition-all ease-out duration-500">
+                  {link.title}
+                </li>
               </Link>
             ))}
           </ul>
