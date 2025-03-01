c15c02acd096b0a72e602d08784872a528730dd4
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2025-03-01 20:11:11 +0200

    more bugs

--- sanity.config.js
+++ sanity.config.js
@@ -15,8 +15,8 @@ import { structure } from "./sanity/structure";
 
 export default defineConfig({
   basePath: "/studio",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId,
+  dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
