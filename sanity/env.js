401ab4280c8fc5e7b1f659d195cf1eaf3fd9273c
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2025-03-01 20:20:40 +0200

    changed dataset to its string

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,5 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-02-28";
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const dataset = "blog";
 export const projectId = "ukcmlqdj";
